91672517b5f0981ad2d81bf6d4bd9b4b308db04c
KuroiiDev <maitsamkadzim26@gmail.com>
2024-09-25 03:23:12 +0700

    update neh

--- register/index.php
+++ register/index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>Register Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -14,59 +13,32 @@
 </head>
 
 
-<?php
-session_start();
-
-if (isset($_SESSION['id'])) {
-    header('Location: /cool-iah');
-    exit;
-}
-include '../config/connection.php';
-
-if (isset($_POST['submit'])) {
-
-    $user = $_POST['user'];
-    $pass = $_POST['pass'];
-
-    $query = "SELECT * FROM accounts WHERE username  = '$user' AND password = '$pass'";
-    $result = mysqli_query($conn, $query);
-
-    if ($result->num_rows > 0) {
-
-        $row = $result->fetch_assoc();
-
-        $_SESSION['name'] = $row['name'];
-        $_SESSION['id'] = $row['id'];
-        header('Location: /cool-iah');
-    } else {
-        echo "Invalid username or password";
-    }
-}
-
-?>
-
 <body>
 <div class="background"></div>
-    <div class="container">
+<div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <h2 class="text-center">Login</h2>
+                <h2 class="text-center">Register</h2>
                 <form method="POST">
                     <div class="form-group">
                         <label for="username">Username:</label>
                         <input type="text" class="form-control" id="username" placeholder="Enter username" name="user">
                     </div>
                     <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name">
+                    </div>
+                    <div class="form-group">
                         <label for="password">Password:</label>
                         <input type="password" class="form-control" id="password" placeholder="Enter password" name="pass">
                     </div>
                     <div class="text-center">
-                        <input type="submit" name="submit" class="btn btn-primary" value="Login"/>
+                        <input type="submit" name="submit" class="btn btn-primary" value="Register"/>
                     </div>
                 </form>
-                <p class="text-center">Don't have an account? <a href="../register">Register</a></p>
+                <p class="text-center">Already have an account? <a href="../login">Login</a></p>
             </div>
         </div>
-    </div>
+    </div> 
 </body>
 </html>
