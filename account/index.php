6c06c1a1ac7d464587cade179ef69c9ecd2df153
KuroiiDev <maitsamkadzim26@gmail.com>
2024-09-27 15:44:16 +0700

    update lagi

---
+++ account/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coliah - Account</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</head>
+
+<?php
+$name = "-";
+session_start();
+
+if (!(isset($_SESSION['id']))) {
+    header("Location: /cooliah/login");
+    exit();
+}
+
+include '../config/connection.php';
+
+$query = "SELECT * FROM users WHERE id = '" . $_SESSION['id'] . "'";
+$result = mysqli_query($conn, $query);
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $name = $row['name'];
+}
+
+?>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cooliah</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../home">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <a href="../account">
+                        <h6 class="mx-2"><?php echo $name; ?></h6>
+                    </a>
+                    <a href="../logout.php" class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container p-3">
+
+    </div>
+</body>
+
+</html>
--- home/index.php
+++ home/index.php
@@ -1,26 +1,30 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cooliah - E-commerce Home Screen</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Cooliah - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles.css">
 </head>
 
-<?php 
+<?php
 $name = "-";
 session_start();
 
-if (!(isset($_SESSION['id']))){
+if (!(isset($_SESSION['id']))) {
     header("Location: /cooliah/login");
     exit();
 }
 
 include '../config/connection.php';
 
-$query = "SELECT * FROM users WHERE id = '".$_SESSION['id']."'";
+$query = "SELECT * FROM users WHERE id = '" . $_SESSION['id'] . "'";
 $result = mysqli_query($conn, $query);
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
@@ -30,32 +34,39 @@ if ($result->num_rows > 0) {
 ?>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Cooliah</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-            <div class="form-inline my-2 my-lg-0">
-                <a href="../account"><h6 class="mx-2"><?php echo $name;?></h6></a>
-                <a href="../logout.php" class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cooliah</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../home">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <a href="../account">
+                        <h6 class="mx-2"><?php echo $name; ?></h6>
+                    </a>
+                    <a href="../logout.php" class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+                </form>
+            </div>
         </div>
     </nav>
-    <div class="jumbotron">
+    <div class="container p-3">
         <h1 class="display-4"></h1>
-        <h1 class="display-4"><span style="color: #000;">Welcome to Cooliah,</span> 
-        <span style="color: #007bff;"><?php echo $name;?></span>!</h1>
+        <h1 class="display-4"><span style="color: #000;">Welcome to Cooliah,</span>
+            <span style="color: #007bff;"><?php echo $name; ?></span>!
+        </h1>
         <p class="lead">Explore our collection of products just for you!</p>
         <hr class="my-4">
         <p>Check out our featured products below:</p>
@@ -76,7 +87,12 @@ if ($result->num_rows > 0) {
             <!-- sampe sini yaakkkkk -->
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
